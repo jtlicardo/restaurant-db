24c32f74a9e4d8faa892a97bfc70d25852d0750d
Josip Tomo Licardo <licardo.josip@gmail.com>
2021-10-25 08:34:47 +0200

    Dodana tablica 'djelatnik'

--- restoran.sql
+++ restoran.sql
@@ -2,6 +2,11 @@ DROP DATABASE IF EXISTS restoran;
 CREATE DATABASE restoran;
 USE restoran;
 
-CREATE TABLE ime_tablice (
-	id INTEGER PRIMARY KEY
+CREATE TABLE djelatnik (
+	id INTEGER PRIMARY KEY,
+    ime VARCHAR(50) NOT NULL,
+    prezime VARCHAR(50) NOT NULL,
+    oib CHAR(11) NOT NULL UNIQUE,
+    broj_mob VARCHAR(10) NOT NULL UNIQUE,
+    datum_zaposlenja DATE NOT NULL
 );
