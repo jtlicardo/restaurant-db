4cdfe2822e90ebdae2ae6180d53a237e688ebaf4
Josip Tomo Licardo <licardo.josip@gmail.com>
2021-10-28 07:52:57 +0200

    Izmjene
    Dodan FK id_djelatnik u tablici 'racun'
    Ispravljeni tipovi podataka stranih ključeva
    Maknut neispravan FK u tablici 'catering'
    Izbrisane tablice 'riba', 'meso', 'voce', 'povrce', 'tekucina'
    Dodani insertovi u tablice 'kategorija_namirnica', 'namirnica' i 'skladiste' kao primjer
    Maknut neispravan FK iz tabl. 'skladiste'
    Maknut atr. 'kolicina_na_zalihi' iz tabl. 'namirnica'
    Dodan atr. 'mjerna_jedinica' u 'skladiste'; maknut atr. 'rok_trajanja'

--- restoran.sql
+++ restoran.sql
@@ -31,14 +31,15 @@ CREATE TABLE stol (
 CREATE TABLE racun (
 	id INTEGER PRIMARY KEY AUTO_INCREMENT,
 	sifra VARCHAR(10) NOT NULL,
-	id_stol INTEGER UNSIGNED NOT NULL,
-	id_djelatnik INTEGER UNSIGNED NOT NULL,
+	id_stol INTEGER NOT NULL,
+	id_djelatnik INTEGER NOT NULL,
 	datum_vrijeme DATETIME NOT NULL,
 	iznos DECIMAL(10, 2) DEFAULT 0.00,
-    FOREIGN KEY (id_stol) REFERENCES stol (id)
+    FOREIGN KEY (id_stol) REFERENCES stol (id),
+    FOREIGN KEY (id_djelatnik) REFERENCES djelatnik (id)
 );
 
-CREATE TABLE alergeni (
+CREATE TABLE alergen (
 	id INTEGER PRIMARY KEY AUTO_INCREMENT,
 	naziv VARCHAR(50) NOT NULL
 );
@@ -54,10 +55,10 @@ CREATE TABLE meni (
 CREATE TABLE stavka_racun (
 	id INTEGER PRIMARY KEY AUTO_INCREMENT,
     id_racun INTEGER NOT NULL,
-    id_stavka INTEGER NOT NULL,
+    id_meni INTEGER NOT NULL,
     kolicina INTEGER NOT NULL,
     FOREIGN KEY (id_racun) REFERENCES racun (id),
-    FOREIGN KEY (id_stavka) REFERENCES stavka (id)
+    FOREIGN KEY (id_meni) REFERENCES meni (id)
 );
 
 CREATE TABLE gost (
@@ -79,72 +80,59 @@ CREATE TABLE catering (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     ponuda VARCHAR(50) NOT NULL,
     cijena DECIMAL(10, 2) NOT NULL,
-    kolicina INTEGER,
-    FOREIGN KEY (kolicina) REFERENCES meni (id)
-);
-    
-CREATE TABLE skladiste (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    ime_robe VARCHAR(50) NOT NULL,
-    kolicina INTEGER,
-    rok_trajanja DATE NOT NULL,
-    FOREIGN KEY (ime_robe) REFERENCES dobavljac(id)
-);
-    
-CREATE TABLE riba(
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    ime_ribe VARCHAR(50) NOT NULL,
-    tezina DECIMAL (10, 3),
-    FOREIGN KEY (ime_ribe) REFERENCES skladiste(id)
-);
-    
-CREATE TABLE meso(
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    ime_mesa VARCHAR(50) NOT NULL,
-    tezina DECIMAL (10, 3),
-    FOREIGN KEY (ime_mesa) REFERENCES skladiste(id)
-);
-    
-CREATE TABLE voce(
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    ime_voca VARCHAR(50) NOT NULL,
-    tezina DECIMAL (10, 3),
-    FOREIGN KEY (ime_voce) REFERENCES skladiste(id)
-);
-    
-CREATE TABLE povrce(
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    ime_povrca VARCHAR(50) NOT NULL,
-    tezina DECIMAL (10, 3),
-    FOREIGN KEY (ime_povrca) REFERENCES skladiste (id)
-);
-    
-CREATE TABLE tekucine(
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    vrsta VARCHAR(50) NOT NULL,
-    kolicina DECIMAL (10, 3),
-    alkohol BOOLEAN,
-    FOREIGN KEY (vrsta) REFERENCES skladiste (id)
-);
-    
-CREATE TABLE stavka_meni (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    id_namirnica INTEGER NOT NULL,
-    kolicina DECIMAL (10, 3),
-    id_meni INTEGER NOT NULL,
-	FOREIGN KEY (id_namirnica) REFERENCES namirnica(id),
-    FOREIGN KEY (id_meni) REFERENCES meni(id)
+    kolicina INTEGER NOT NULL
 );
     
 CREATE TABLE kategorija_namirnica (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     naziv VARCHAR(50) NOT NULL
 );
+
+INSERT INTO kategorija_namirnica VALUES
+	(1, "Riba"),
+    (2, "Meso"),
+    (3, "Voće"),
+    (4, "Povrće"),
+    (5, "Piće");
     
 CREATE TABLE namirnica (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     naziv VARCHAR(50) NOT NULL,
-    kolicina_na_zalihi DECIMAL (10, 3),
     id_kategorija INTEGER NOT NULL,
-    FOREIGN KEY (id_kategorija) REFERENCES kategorija_namirnica(id)
+    FOREIGN KEY (id_kategorija) REFERENCES kategorija_namirnica (id)
 );
+
+INSERT INTO namirnica VALUES
+	(1, "Orada", 1),
+    (2, "Biftek", 2),
+    (3, "Jagoda", 3),
+    (4, "Vino Teran", 5),
+    (5, "Vino Malvazija", 5);
+
+CREATE TABLE stavka_meni (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id_namirnica INTEGER NOT NULL,
+    kolicina DECIMAL (10, 3),
+    id_meni INTEGER NOT NULL,
+	FOREIGN KEY (id_namirnica) REFERENCES namirnica (id),
+    FOREIGN KEY (id_meni) REFERENCES meni (id)
+);
+    
+CREATE TABLE skladiste (
+	id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id_namirnica INTEGER NOT NULL,
+    kolicina INTEGER NOT NULL,
+    mjerna_jedinica VARCHAR(20) NOT NULL,
+    FOREIGN KEY (id_namirnica) REFERENCES namirnica (id)
+);
+
+INSERT INTO skladiste VALUES
+	(1, 1, 30, "komada"),
+    (2, 2, 50, "komada"),
+    (3, 3, 4, "kilograma"),
+    (4, 4, 60, "litara"),
+    (5, 5, 60, "litara");
+    
+    
+    
+    
